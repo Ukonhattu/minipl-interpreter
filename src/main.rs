084fd5cc59a9c3b72d0180f53c4499d3607ff479
Daniel Koch <daniel.koch@helsinki.fi>
2022-02-09 16:02:10 +0200

    clippy fix

--- src/main.rs
+++ src/main.rs
@@ -7,6 +7,6 @@ fn main() {
     let args: Vec<String> = env::args().collect();
     let filename = &args[1];
     let contents = fs::read_to_string(filename).expect("Something went wrong reading the file");
-    let scanner = Scanner::new(contents);
+    let _scanner = Scanner::new(contents);
     
 }
