4f14b3283b9f8a282d430cc8b1ad8626161239e2
Daniel Koch <daniel.koch@helsinki.fi>
2022-02-09 16:01:08 +0200

    partially implemented token checking

--- src/scanner/scanner.rs
+++ src/scanner/scanner.rs
@@ -1,12 +1,36 @@
 pub struct Scanner {
-    program: String
+    program: Vec<char>,
+    read_index: usize
 }
 
 impl Scanner {
     
     pub fn new(program: String) -> Self {
         Self {
-            program: program
+            program: program.chars().collect(),
+            read_index: 0
         }
     }
+    pub fn scan(&self) {
+        let left = 0; 
+        let right = 0;
+        let len = self.program.len();
+
+        while right <= len && left <= right {
+            
+        }
+    }
+
+    fn get_next_character(&mut self) -> Option<&char> {
+        let char = self.program.get(self.read_index);
+        self.read_index += 1;
+        char 
+
+    }
+
+    fn peek_next_character(&self) -> Option<&char> {
+        self.program.get(self.read_index + 1)
+    }
+
+
 }
